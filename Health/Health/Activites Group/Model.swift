3960c6a2a0f51a868230dd99b6e74ae568af7abf
Aziz <Aziz.programmer1@gmail.com>
2021-12-12 09:44:29 +0300

    add new videos

--- Health/Health/Activites Group/Model.swift
+++ Health/Health/Activites Group/Model.swift
@@ -27,8 +27,17 @@ struct Video {
     let v8 = Video(thumbnailFileName: "v8", thumbImage: "v8")
     let v9 = Video(thumbnailFileName: "v9", thumbImage: "v9")
     let v10 = Video(thumbnailFileName: "v10", thumbImage: "v10")
-     
-    
-    return [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10]
+    let v11 = Video(thumbnailFileName: "v11", thumbImage: "v11")
+    let v12 = Video(thumbnailFileName: "v12", thumbImage: "v12")
+    let v13 = Video(thumbnailFileName: "v13", thumbImage: "v13")
+    let v14 = Video(thumbnailFileName: "v14", thumbImage: "v14")
+    let v15 = Video(thumbnailFileName: "v15", thumbImage: "v15")
+    let v16 = Video(thumbnailFileName: "v16", thumbImage: "v16")
+    let v17 = Video(thumbnailFileName: "v17", thumbImage: "v17")
+    let v18 = Video(thumbnailFileName: "v18", thumbImage: "v18")
+    let v19 = Video(thumbnailFileName: "v19", thumbImage: "v19")
+    let v20 = Video(thumbnailFileName: "v20", thumbImage: "v20")
+    let v21 = Video(thumbnailFileName: "v21", thumbImage: "v21")
+    return [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13,v14,v15, v16, v17, v18, v19, v20, v21]
 }
 }
---
+++ Health/Health/File.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Health
+//
+//  Created by Abdulaziz on 07/05/1443 AH.
+//
+
+import Foundation
+import UIKit
+
+class TabBar: UITabBarController{
+    
+    @IBOutlet weak var tab: UITabBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tab.layer.cornerRadius = 35
+        
+    }
+    
+    
+}
